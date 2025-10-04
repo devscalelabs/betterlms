bf9e034cf7eba79a940ebbf206f04c61144dd63c
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-10-04 21:37:11 +0700

    Update .gitignore, package.json, and database scripts; adjust Prisma schema output path

---
+++ packages/database/prisma/migrations/20251004143603_init_db/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'MENTOR', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "bio" TEXT,
+    "isEmailVerified" BOOLEAN,
+    "imageUrl" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "isSuspended" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_username_idx" ON "User"("username");
