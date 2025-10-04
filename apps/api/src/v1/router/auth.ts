8006b28db48d20a50078b6bb205147d51cfb1463
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-10-04 23:41:38 +0700

    Add @elysiajs/jwt dependency and implement JWT authentication in authRouter; update package.json and pnpm-lock.yaml accordingly.

--- apps/api/src/v1/router/auth.ts
+++ apps/api/src/v1/router/auth.ts
@@ -1,9 +1,16 @@
 import { prisma } from "@betterlms/database";
+import { jwt } from "@elysiajs/jwt";
 import bcrypt from "bcrypt";
 import { Elysia, t } from "elysia";
 
 export const authRouter = new Elysia({ prefix: "/auth" })
 	.decorate("db", prisma)
+	.use(
+		jwt({
+			name: "jwt",
+			secret: "Fischl von Luftschloss Narfidort",
+		}),
+	)
 	.post(
 		"/register",
 		async ({ body, db, status }) => {
@@ -43,6 +50,43 @@ export const authRouter = new Elysia({ prefix: "/auth" })
 					format: "regex",
 					pattern: "^[a-zA-Z0-9]+$",
 				}),
+				email: t.String({ format: "email" }),
+				password: t.String({ minLength: 8 }),
+			}),
+		},
+	)
+	.post(
+		"/login",
+		async ({ body, db, status }) => {
+			const { email, password } = body;
+
+			const user = await db.user.findUnique({
+				where: {
+					email,
+				},
+			});
+
+			if (!user) {
+				return status(401, {
+					error: "Invalid credentials",
+				});
+			}
+
+			// This is safe, because this API is only for credential login
+			const isPasswordValid = await bcrypt.compare(password, user.password!);
+
+			if (!isPasswordValid) {
+				return status(401, {
+					error: "Invalid credentials",
+				});
+			}
+
+			return status(200, {
+				user,
+			});
+		},
+		{
+			body: t.Object({
 				email: t.String({ format: "email" }),
 				password: t.String({ minLength: 8 }),
 			}),
