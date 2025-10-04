ae38e5b7dde8c7f0d3107468a79fba4e5e26fa79
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-10-04 22:17:31 +0700

    Enhance VSCode settings by enabling format on save for JavaScript, TypeScript, and JSON files, and adjust import order in index.tsx for better readability.

--- apps/platform/src/app/index.tsx
+++ apps/platform/src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Providers } from "./providers";
 import { Button } from "@betterlms/ui";
+import { Providers } from "./providers";
 
 export const App = () => {
 	return (
 		<Providers>
 			<div>
-					<div>Hello!</div>
+				<div>Hello!</div>
 				<Button>Hello!</Button>
 			</div>
 		</Providers>
