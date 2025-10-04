9b5e74edf773b7c2b611030a1885fabb38b11bb3
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-10-04 22:36:32 +0700

    Update package.json and pnpm-lock.yaml to add @elysiajs/cors as a dependency, modify dev script for parallel execution, and ensure compatibility with the API app's dependencies.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,11 @@
+import { cors } from "@elysiajs/cors";
 import { node } from "@elysiajs/node";
 import { Elysia } from "elysia";
 
 const PORT = process.env.BACKEND_PORT || 8000;
 
 export const app = new Elysia({ adapter: node() })
+	.use(cors())
 	.get("/", () => "Hello Elysia")
 	.listen(PORT, ({ hostname, port }) => {
 		console.log(`🦊 Elysia is running at ${hostname}:${port}`);
